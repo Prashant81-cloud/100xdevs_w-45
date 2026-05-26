25a5e19940cce2f7434ce88846ed896efb598fc4
Prashant81-cloud <prashant81046@gmail.com>
2026-05-26 20:39:16 +0530

    ci to production

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,8 +5,8 @@ export default async function Home() {
 
     return (
         <div>
-            {user?.username}
-            {user?.password}
+            production-username:- {user?.username}
+            production-password:-{user?.password}
         </div>
     );
 }
